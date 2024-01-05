3d21adb1f6b414d2e06744dde33298d401595c82
erayerm <ermemiseray@gmail.com>
2024-01-06 00:06:40 +0300

    switch route on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Main from "./pages/Main";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 const App = () => {
   return (
     <>
-      <Main></Main>
+      <Switch>
+        <Route path="/pizza">
+    
+        </Route>
+        <Route path="/">
+          <Main></Main>
+        </Route>
+      </Switch>
+      
+      
     </>
   );
 };
