f0eac7b4f7243c6fa8744379ed178d95483be2fe
erayerm <ermemiseray@gmail.com>
2024-01-05 23:30:55 +0300

    Main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,55 @@
 import React from "react"
-import HeaderSection from "../components/HeaderSection"
-export default function Main(){
-    return(
+import banner from "../assets/mvp-banner.png"
+import styled from "styled-components"
+import logo from "../assets/logo.svg"
+
+export default function Main() {
+    const Image = styled.img`
+        position: absolute;
+        top: 0px;
+        z-index: -1;
+        width: 100vw;
+        height: 100vh;
+        object-fit: cover;
+    `;
+
+    const Container = styled.div`
+        position: relative;
+        z-index: 1;
+        display: flex;
+        flex-direction: column;
+        align-items:center;
+        width: 30vw;
+        min-width: 636px;
+        margin: 10vh auto 0 auto;
+    `;
+
+    const Title = styled.h1`
+        font-size: 86px;
+        font-family: 'Barlow', sans-serif;
+        color: #FFFFFF;
+        text-align: center;
+    `;
+    const Button = styled.button`
+        font-size: 18px;
+        background-color: #FDC913;
+        color: #292929;
+        border-radius: 50px;
+        padding: 10px 50px;
+        border: none;
+    `;
+
+    return (
         <>
-            <HeaderSection/>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@100&display=swap');
+            </style>
+            <Container>
+                <img src={logo} className="logo react" alt="React logo" />
+                <Title>KOD ACIKTIRIR PIZZA, DOYURUR</Title>
+                <Button>ACIKTIM</Button>
+            </Container>
+            <Image src={banner} alt="pizza" />
         </>
     )
 }
