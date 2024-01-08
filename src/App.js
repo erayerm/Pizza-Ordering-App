59df1c89b5ab5023c9071d242b238133948174ff
erayerm <ermemiseray@gmail.com>
2024-01-08 17:03:31 +0300

    adding Success Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Main from "./pages/Main";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import Order from "./pages/Order";
+import Success from "./pages/Success";
 const App = () => {
   return (
     <>
       <Switch>
         <Route path="/pizza">
           <Order/>
+        </Route>
+        <Route path="/success">
+          <Success/>
         </Route>
         <Route path="/">
           <Main/>
---
+++ src/pages/Success.jsx
@@ -0,0 +1,53 @@
+
+import React from "react"
+import styled from "styled-components"
+import logo from "../assets/logo.svg"
+
+const Container = styled.div`
+    width:100vw;
+    height: 100vh;
+    background-color:#CE2829;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+`;
+const Logo = styled.div`
+    flex: 2;
+    padding-top: 120px;
+`;
+
+const Text = styled.p`
+    flex: 6;
+    color: #FFF;
+    text-align: center;
+    font-family: Barlow;
+    font-size: 86px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 92px; /* 106.977% */
+    letter-spacing: -8px;
+    text-transform: uppercase;
+`;
+
+
+export default function Success() {
+
+    
+
+    return (
+        <>
+            <Container>
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300&display=swap');
+                </style>
+                <Logo>
+                    <img src={logo} alt="logo" />
+                </Logo>
+                <Text>
+                    TEBRİKLER!<br />SİPARİŞİNİZ ALINDI!
+                </Text>
+            </Container>
+        </>
+    )
+}
