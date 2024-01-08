b39b06cbff5c644f2c49078d52dfa3490fae0a47
erayerm <ermemiseray@gmail.com>
2024-01-08 22:30:55 +0300

    redo main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,41 +4,38 @@ import styled from "styled-components"
 import logo from "../assets/logo.svg"
 import { Link } from "react-router-dom";
 
-export default function Main() {
-    const Image = styled.img`
-        position: absolute;
-        top: 0px;
-        z-index: -1;
-        width: 100vw;
-        height: 100vh;
-        object-fit: cover;
+const Title = styled.h1`
+        color: #FFF;
+        font-family: "Roboto Condensed";
+        font-size: 86px;
+        font-style: normal;
+        font-weight: 200;
+        text-transform: uppercase;
+        text-align: center;
+        margin:0;
+        margin-top: 35px;
+        margin-bottom: 17px;
+        letter-spacing: 1.5px;
+
     `;
 
-    const Container = styled.div`
-        position: relative;
-        z-index: 1;
+const Container = styled.div`
+    width:100vw;
+    height:100vh;
+    background-color: red;
+    background-image: url(${banner});
+`;
+const ItemsContainer = styled.div`
         display: flex;
         flex-direction: column;
         align-items:center;
         width: 30vw;
         min-width: 636px;
-        margin: 10vh auto 0 auto;
+        margin: 0 auto;
+        padding-top: 120px;
     `;
 
-    const Title = styled.h1`
-        color: #FFF;
-        font-family: "Barlow";
-        font-size: 86px;
-        font-style: normal;
-        font-weight: 200;
-        line-height: 92px;
-        letter-spacing: -5px;
-        text-transform: uppercase;
-        text-align: center;
-        margin-bottom: 0;
-    `;
-
-    const Button = styled.button`
+const Button = styled.button`
         font-size: 18px;
         background-color: #FDC913;
         color: #292929;
@@ -48,19 +45,27 @@ export default function Main() {
         margin-top: 20px;
     `;
 
+const Logo = styled.img`
+        margin: auto 0;
+    `;
+
+export default function Main() {
     return (
         <>
             <style>
-                @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300&display=swap');
+                @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap');
             </style>
             <Container>
-                <img src={logo} alt="logo" />
-                <Title>KOD ACIKTIRIR PIZZA, DOYURUR</Title>
-                <Link to="/pizza">
-                    <Button id="order-pizza">ACIKTIM</Button>
-                </Link>
+                <ItemsContainer>
+                    <Logo src={logo} alt="logo" />
+                    <Title>
+                        KOD ACIKTIRIR PİZZA, DOYURUR
+                    </Title>
+                    <Link to="/pizza">
+                        <Button id="order-pizza">ACIKTIM</Button>
+                    </Link>
+                </ItemsContainer>
             </Container>
-            <Image src={banner} alt="pizza" />
         </>
     )
 }
